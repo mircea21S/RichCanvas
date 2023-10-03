584eb2b31c7a423ceb11e5e31f7d9bee4be51fcd
Mircea Staicu <staicumircea21@gmail.com>
2023-10-03 15:43:22 +0300

    StateManager small refactor + Zoom refactor + Added Commands and CommandBindings StateManger different way of registering and managing state selection and execution

--- RichCanvas/Gestures/RichCanvasGestures.cs
+++ RichCanvas/Gestures/RichCanvasGestures.cs
@@ -7,6 +7,9 @@ namespace RichCanvas.Gestures
         public static InputGesture Select { get; } = new MouseGesture(MouseAction.LeftClick);
         public static InputGesture Drawing { get; } = new MouseGesture(MouseAction.LeftClick);
         public static InputGesture Drag { get; } = new MouseGesture(MouseAction.LeftClick);
+        public static InputGesture ZoomIn { get; } = new KeyGesture(Key.OemPlus, ModifierKeys.Control);
+        public static InputGesture ZoomOut { get; } = new KeyGesture(Key.OemMinus, ModifierKeys.Control);
         public static InputGesture Pan { get; set; } = new MouseKeyGesture(new MouseGesture(MouseAction.LeftClick), new KeyGesture(Key.Space));
+        public static ModifierKeys Zoom { get; set; } = ModifierKeys.Control;
     }
 }
