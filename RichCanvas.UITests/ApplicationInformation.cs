37385ac3734879271ef5b8e0506e43715d5d733c
Mircea Staicu <staicumircea21@gmail.com>
2023-11-14 16:40:11 +0200

    Added UI automation test project, using FlaUI nuget + exposed AutomationPeer for RichItemsControl and RichItemContainer

---
+++ RichCanvas.UITests/ApplicationInformation.cs
@@ -0,0 +1,8 @@
+﻿namespace RichCanvas.UITests
+{
+    internal class ApplicationInformation
+    {
+        internal const string DemoBinPath = @"C:\Programming Projects\RichCanvas\RichCanvasDemo\bin\Debug\netcoreapp3.1\RichCanvasDemo.exe\";
+        internal const string DemoExeProcessName = "RichCanvasDemo.exe";
+    }
+}
