aca23dbead7f5e1494927a68c27de35b29c90d7f
Mircea Staicu <staicumircea21@gmail.com>
2023-11-27 14:12:02 +0200

    UI tests updates: -added more information on Value Pattern data -added extension methods for Window and Application classes for reusability and cleaner code -added ui tests for DrawingState -started RichItemContainerTests class -modified demo app to support testing

--- RichCanvas/Automation/ControlInformations/RichItemContainerData.cs
+++ RichCanvas/Automation/ControlInformations/RichItemContainerData.cs
@@ -3,5 +3,7 @@
     public class RichItemContainerData
     {
         public bool IsSelected { get; set; }
+        public double ScaleX { get; set; }
+        public double ScaleY { get; set; }
     }
 }
