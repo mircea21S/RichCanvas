d8cb01e20689a83113b03360871d965299d9c90d
Mircea Staicu <staicumircea21@gmail.com>
2024-05-31 11:00:15 +0300

    Added new methods on CanvasState to support state switching + made possible select and pan states switch + updated MouseCustomKeyGesture to match the current input correctly

--- RichCanvas/States/CanvasState.cs
+++ RichCanvas/States/CanvasState.cs
@@ -3,23 +3,51 @@ using System.Windows.Input;
 
 namespace RichCanvas.States
 {
-    /// <summary>
-    /// TODO: Excalidraw diagram
-    /// </summary>
+    /// <summary>The base class for editor states.</summary>
     public abstract class CanvasState
     {
+        /// <summary>The owner of the state.</summary>
         protected RichItemsControl Parent { get; }
+        /// <summary>
+        /// Constructs a new <see cref="CanvasState "/>.
+        /// </summary>
+        /// <param name="parent">The owner of the state.</param>
         public CanvasState(RichItemsControl parent)
         {
             Parent = parent;
         }
+        /// <summary>
+        /// Called whenever <see cref="RichItemsControl.PushState(CanvasState)"/> is called (becomes the <see cref="RichItemsControl.CurrentState"/>).
+        /// <br />
+        /// Note: <i>Used to initialize the State before any input is processed by it.</i>
+        /// </summary>
         public virtual void Enter() { }
-        public virtual void Cancel() { }
+        /// <summary>
+        /// Called whenever <see cref="RichItemsControl.PopState()"/> is called.
+        /// <br />
+        /// Note: <i>Used whenever a state switch happens in order to update the state which was suspended.</i>
+        /// </summary>
+        public virtual void ReEnter() { }
+        /// <summary>
+        /// Called whenever <see cref="RichItemsControl.PopState()"/> is called.
+        /// </summary>
+        public virtual void Exit() { }
+        /// <inheritdoc cref="RichItemsControl.OnMouseDown(MouseButtonEventArgs)"/>
         public virtual void HandleMouseDown(MouseButtonEventArgs e) { }
+        /// <inheritdoc cref="RichItemsControl.OnMouseMove(MouseEventArgs)"/>
         public virtual void HandleMouseMove(MouseEventArgs e) { }
+        /// <inheritdoc cref="RichItemsControl.OnMouseUp(MouseButtonEventArgs)"/>
         public virtual void HandleMouseUp(MouseButtonEventArgs e) { }
+        /// <inheritdoc cref="RichItemsControl.OnKeyDown(KeyEventArgs)"/>
+        public virtual void HandleKeyDown(KeyEventArgs e) { }
+        /// <inheritdoc cref="RichItemsControl.OnKeyUp(KeyEventArgs)"/>
+        public virtual void HandleKeyUp(KeyEventArgs e) { }
+        /// <summary>Handles auto panning when mouse is outside the canvas.</summary>
         public virtual void HandleAutoPanning(Point mousePosition, bool heightChanged = false) { }
+        /// <summary>Pushes a new state into the stack.</summary>
+        /// <param name="state">The new state.</param>
         public virtual void PushState(CanvasState state) => Parent.PushState(state);
+        /// <summary>Pops the current state from the stack.</summary>
         public virtual void PopState() => Parent.PopState();
     }
 }
