41069829c59fddc0564264fd8752f62db6591b13
Mircea Staicu <staicumircea21@gmail.com>
2024-07-13 22:14:26 +0300

    Added single selection UI automation tests when real-time selection is disabled.

--- RichCanvas.UITests/Tests/SingleSelectionStateTests.cs
+++ RichCanvas.UITests/Tests/SingleSelectionStateTests.cs
@@ -111,7 +111,82 @@ namespace RichCanvas.UITests.Tests
 
             Mouse.Click(RichItemContainerModelMocks.SingleSelectionRealTimeDragTestItems[2].Center.AsDrawingPoint().ToCanvasDrawingPoint());
             RichItemsControl.SelectedItem.Should().Be(RichItemsControl.Items[2]);
+        }
 
+        [Test]
+        public void SingleSelectionStateDrag_WithRealTimeSelectionDisabled_ShouldSelectNothingWhileMouseMoves()
+        {
+            // arrange
+            // add items for selection
+            Window.InvokeButton(AutomationIds.AddTestSingleSelectionItemsButtonId);
+            Wait.UntilInputIsProcessed();
+
+            // act and assert
+            Mouse.Position = new Point(30, 30);
+            Mouse.Down();
+            Mouse.Position = new Point(100, 100);
+            RichItemsControl.SelectedItem.Should().BeNull();
+            RichItemsControl.SelectedItems.Should().BeEmpty();
+
+            Mouse.Position = new Point(200, 200);
+            RichItemsControl.SelectedItem.Should().BeNull();
+            RichItemsControl.SelectedItems.Should().BeEmpty();
+
+            Mouse.Position = new Point(300, 300);
+            RichItemsControl.SelectedItem.Should().BeNull();
+            RichItemsControl.SelectedItems.Should().BeEmpty();
+
+            Mouse.Up();
+        }
+
+        [Test]
+        public void SingleSelectionStateDrag_WithRealTimeSelectionDisabled_ShouldSelectOneItemOnMouseUp()
+        {
+            // arrange
+            // add items for selection
+            Window.InvokeButton(AutomationIds.AddTestSingleSelectionItemsButtonId);
+            Wait.UntilInputIsProcessed();
+
+            // act
+            Mouse.Drag(new Point(30, 30), new Point(300, 300));
+
+            // assert
+            RichItemsControl.SelectedItem.Should().NotBeNull();
+            RichItemsControl.SelectedItems.Length.Should().Be(1);
+        }
+
+        [Test]
+        public void SingleSelectionStateDrag_WithRealTimeSelectionDisabled_ShouldSelectLastItemAddedToItemsSourceOnMouseUp()
+        {
+            // arrange
+            // add items for selection
+            Window.InvokeButton(AutomationIds.AddTestSingleSelectionItemsButtonId);
+            Wait.UntilInputIsProcessed();
+
+            // act
+            Mouse.Drag(new Point(30, 30), new Point(300, 300));
+
+            // assert
+            RichItemsControl.SelectedItem.Should().Be(RichItemsControl.Items[2]);
+        }
+
+        [Test]
+        public void SingleSelectionStateDrag_WithRealTimeSelectionDisabled_ShouldSelectEachClickedItem()
+        {
+            // arrange
+            // add items for selection
+            Window.InvokeButton(AutomationIds.AddTestSingleSelectionItemsButtonId);
+            Wait.UntilInputIsProcessed();
+
+            // act and assert
+            Mouse.Click(RichItemContainerModelMocks.SingleSelectionRealTimeDragTestItems[0].Center.AsDrawingPoint().ToCanvasDrawingPoint());
+            RichItemsControl.SelectedItem.Should().Be(RichItemsControl.Items[0]);
+
+            Mouse.Click(RichItemContainerModelMocks.SingleSelectionRealTimeDragTestItems[1].Center.AsDrawingPoint().ToCanvasDrawingPoint());
+            RichItemsControl.SelectedItem.Should().Be(RichItemsControl.Items[1]);
+
+            Mouse.Click(RichItemContainerModelMocks.SingleSelectionRealTimeDragTestItems[2].Center.AsDrawingPoint().ToCanvasDrawingPoint());
+            RichItemsControl.SelectedItem.Should().Be(RichItemsControl.Items[2]);
         }
 
         private void ArrangeRealTimeScenario()
