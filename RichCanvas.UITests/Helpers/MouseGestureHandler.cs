b18074a8097dc655fc41c4596a964461c6cf3978
Mircea Staicu <staicumircea21@gmail.com>
2024-07-17 23:52:05 +0300

    Use gestures throughout all SelectingState tests + fix bug when clearing ItemsSource SelectedItems wasn't cleared + add exception handling to UI Tests App to see the exception when thrown in automated tests

--- RichCanvas.UITests/Helpers/MouseGestureHandler.cs
+++ RichCanvas.UITests/Helpers/MouseGestureHandler.cs
@@ -1,5 +1,4 @@
 ﻿using FlaUI.Core.Input;
-using FlaUI.Core.WindowsAPI;
 using System;
 using System.Drawing;
 
@@ -16,21 +15,11 @@ namespace RichCanvas.UITests.Helpers
 
         internal override void Click(Point point)
         {
-            VirtualKeyShort key = VirtualKeyShort.KEY_X;
-            if (_mouseGesture.Modifiers != System.Windows.Input.ModifierKeys.None)
-            {
-                key = _mouseGesture.Modifiers.ToVirtualKeyShort();
-            }
+            var pressedKey = _mouseGesture.Modifiers.ToVirtualKeyShort();
 
-            if (key != VirtualKeyShort.KEY_X)
-            {
-                Keyboard.Press(key);
-            }
+            Keyboard.Press(pressedKey);
             Mouse.Click(point, _mouseGesture.MouseAction.ToMouseButton());
-            if (key != VirtualKeyShort.KEY_X)
-            {
-                Keyboard.Release(key);
-            }
+            Keyboard.Release(pressedKey);
         }
 
         internal override void DefferedDrag(Point startPoint, params (Point Position, Action StepAction)[] dragStepPoints)
