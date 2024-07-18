0ed868b62709de9d2860b7d8e2eecb3d6a22c491
Mircea Staicu <staicumircea21@gmail.com>
2024-07-18 18:15:51 +0300

    Refactor UI automated tests App to separte all logic needed for testing into different view models.

---
+++ RichCanvasUITests.App/States/SingleSelectionStateViewModel.cs
@@ -0,0 +1,26 @@
+﻿using RichCanvasUITests.App.TestMocks;
+using System.Windows.Input;
+
+namespace RichCanvasUITests.App.States
+{
+    public class SingleSelectionStateViewModel
+    {
+        public MainWindowViewModel Parent { get; }
+
+        private ICommand _addSingleSelectionItemsForTest;
+        public ICommand AddSingleSelectionItemsForTest => _addSingleSelectionItemsForTest ??= new RelayCommand(AddTestSingleSelectionItems);
+
+        public SingleSelectionStateViewModel(MainWindowViewModel parent)
+        {
+            Parent = parent;
+        }
+
+        private void AddTestSingleSelectionItems()
+        {
+            foreach (var item in SingleSelectionStateDataMocks.SingleSelectionRealTimeDragTestItems)
+            {
+                Parent.Items.Add(item);
+            }
+        }
+    }
+}
---
+++ RichCanvasUITests.App/TestMocks/MultipleSelectionStateDataMocks.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace RichCanvasUITests.App.TestMocks
+{
+    public static class MultipleSelectionStateDataMocks
+    {
+        public static List<RichItemContainerModel> PositionedSelectableItemsListMock =>
+        [
+            new()
+            {
+                Top = 50,
+                Left = 50,
+                Height = 50,
+                Width = 50
+            },
+            new()
+            {
+                Top = 150,
+                Left = 150,
+                Height = 50,
+                Width = 50
+            }
+        ];
+    }
+}
