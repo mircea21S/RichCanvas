d3a1bdcace9551e22b37becf1b1e6eeb1826f534
Mircea Staicu <staicumircea21@gmail.com>
2024-07-18 18:15:51 +0300

    Refactor UI automated tests App to separte all logic needed for testing into different view models.

--- RichCanvasUITests.App/MainWindowViewModel.cs
+++ RichCanvasUITests.App/MainWindowViewModel.cs
@@ -1,6 +1,4 @@
-﻿using RichCanvasUITests.App.Models;
-using RichCanvasUITests.App.TestMocks;
-using System;
+﻿using RichCanvasUITests.App.States;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Windows.Input;
@@ -10,116 +8,50 @@ namespace RichCanvasUITests.App
     public class MainWindowViewModel : ObservableObject
     {
         public ObservableCollection<RichItemContainerModel> Items { get; }
+        public ObservableCollection<RichItemContainerModel> SelectedItems { get; }
 
         private RichItemContainerModel _selectedItem;
         public RichItemContainerModel SelectedItem { get => _selectedItem; set => SetProperty(ref _selectedItem, value); }
 
+        private RelayCommand _clearAllItemsCommand;
+        public ICommand ClearAllItemsCommand => _clearAllItemsCommand ??= new RelayCommand(ClearAllItems);
+
+        private RelayCommand<NotifyCollectionChangedAction> _updateItemsSourceCommand;
+        public ICommand UpdateItemsSourceCommand => _updateItemsSourceCommand ??= new RelayCommand<NotifyCollectionChangedAction>(UpdateItemsSource);
+
+        private RelayCommand _switchItemsPositionCommand;
+        public ICommand SwitchItemsPositionCommand => _switchItemsPositionCommand ??= new RelayCommand(SwitchItemsPosition);
+
+        private bool _realTimeSelectionEnabled;
+        public bool RealTimeSelectionEnabled
+        {
+            get => _realTimeSelectionEnabled;
+            set => SetProperty(ref _realTimeSelectionEnabled, value);
+        }
+
+        private bool _canSelectMultipleItems;
+        public bool CanSelectMultipleItems
+        {
+            get => _canSelectMultipleItems;
+            set => SetProperty(ref _canSelectMultipleItems, value);
+        }
+
+        public SingleSelectionStateViewModel SingleSelectionState { get; }
+        public MultipleSelectionStateViewModel MultipleSelectionState { get; }
+        public DrawingStateViewModel DrawingState { get; }
+
         public MainWindowViewModel()
         {
-            Items = new ObservableCollection<RichItemContainerModel>();
-            SelectedItems = new ObservableCollection<RichItemContainerModel>();
-        }
-
-        private RelayCommand<bool> _addPositionedRectangleCommand;
-
-        public ICommand AddPositionedRectangleCommand
-        {
-            get
-            {
-                if (_addPositionedRectangleCommand == null)
-                {
-                    _addPositionedRectangleCommand = new RelayCommand<bool>(AddPositionedRectangle);
-                }
-
-                return _addPositionedRectangleCommand;
-            }
-        }
-
-        private void AddPositionedRectangle(bool isImmutable)
-        {
-            Items.Add(isImmutable ? RichItemContainerModelMocks.ImmutablePositionedRectangleMock : RichItemContainerModelMocks.PositionedRectangleMock);
-        }
-
-        private RelayCommand _clearAllItemsCommand;
-
-        public ICommand ClearAllItemsCommand
-        {
-            get
-            {
-                if (_clearAllItemsCommand == null)
-                {
-                    _clearAllItemsCommand = new RelayCommand(ClearAllItems);
-                }
-
-                return _clearAllItemsCommand;
-            }
+            Items = [];
+            SelectedItems = [];
+            SingleSelectionState = new SingleSelectionStateViewModel(this);
+            MultipleSelectionState = new MultipleSelectionStateViewModel(this);
+            DrawingState = new DrawingStateViewModel(this);
         }
 
         private void ClearAllItems()
         {
             Items.Clear();
-        }
-
-        private RelayCommand _addDrawnRectangleCommand;
-
-        public ICommand AddDrawnRectangleCommand
-        {
-            get
-            {
-                if (_addDrawnRectangleCommand == null)
-                {
-                    _addDrawnRectangleCommand = new RelayCommand(AddDrawnRectangle);
-                }
-
-                return _addDrawnRectangleCommand;
-            }
-        }
-
-        private void AddDrawnRectangle()
-        {
-            Items.Add(RichItemContainerModelMocks.DrawnRectangleMock);
-        }
-
-        private RelayCommand<Type> _addEmptyItemCommand;
-
-        public ICommand AddEmptyItemCommand
-        {
-            get
-            {
-                if (_addEmptyItemCommand == null)
-                {
-                    _addEmptyItemCommand = new RelayCommand<Type>(AddEmptyRectangle);
-                }
-
-                return _addEmptyItemCommand;
-            }
-        }
-
-        private void AddEmptyRectangle(Type itemType)
-        {
-            if (itemType == typeof(RichItemContainerModel))
-            {
-                Items.Add(new RichItemContainerModel());
-            }
-            else if (itemType == typeof(Line))
-            {
-                Items.Add(new Line());
-            }
-        }
-
-        private RelayCommand<NotifyCollectionChangedAction> _updateItemsSourceCommand;
-
-        public ICommand UpdateItemsSourceCommand
-        {
-            get
-            {
-                if (_updateItemsSourceCommand == null)
-                {
-                    _updateItemsSourceCommand = new RelayCommand<NotifyCollectionChangedAction>(UpdateItemsSource);
-                }
-
-                return _updateItemsSourceCommand;
-            }
         }
 
         private void UpdateItemsSource(NotifyCollectionChangedAction actionType)
@@ -134,88 +66,10 @@ namespace RichCanvasUITests.App
             }
         }
 
-        private RelayCommand _testCommand;
-
-        public ICommand TestCommand
-        {
-            get
-            {
-                if (_testCommand == null)
-                {
-                    _testCommand = new RelayCommand(Test);
-                }
-
-                return _testCommand;
-            }
-        }
-
-        private void Test()
+        private void SwitchItemsPosition()
         {
             // changable method to test stuff
             Items[0] = Items[1];
-        }
-
-        private RelayCommand _addSelectableItemsCommmand;
-
-        public ICommand AddSelectableItemsCommmand
-        {
-            get
-            {
-                if (_addSelectableItemsCommmand == null)
-                {
-                    _addSelectableItemsCommmand = new RelayCommand(AddSelectableItems);
-                }
-
-                return _addSelectableItemsCommmand;
-            }
-        }
-
-        private void AddSelectableItems()
-        {
-            foreach (var item in RichItemContainerModelMocks.PositionedSelectableItemsListMock)
-            {
-                Items.Add(item);
-            }
-        }
-
-        public ObservableCollection<RichItemContainerModel> SelectedItems { get; }
-
-        private bool _realTimeSelectionEnabled;
-
-        public bool RealTimeSelectionEnabled
-        {
-            get => _realTimeSelectionEnabled;
-            set => SetProperty(ref _realTimeSelectionEnabled, value);
-        }
-
-        private bool _canSelectMultipleItems;
-
-        public bool CanSelectMultipleItems
-        {
-            get => _canSelectMultipleItems;
-            set => SetProperty(ref _canSelectMultipleItems, value);
-        }
-
-        private ICommand _addSingleSelectionItemsForTest;
-        public ICommand AddSingleSelectionItemsForTest
-        {
-            get
-            {
-                if (_addSingleSelectionItemsForTest == null)
-                {
-                    _addSingleSelectionItemsForTest = new RelayCommand(AddTestSingleSelectionItems);
-                }
-
-                return _addSingleSelectionItemsForTest;
-            }
-        }
-
-        private void AddTestSingleSelectionItems()
-        {
-            foreach (var item in RichItemContainerModelMocks.SingleSelectionRealTimeDragTestItems)
-            {
-                Items.Add(item);
-            }
         }
     }
 }
---
+++ RichCanvasUITests.App/States/MultipleSelectionStateViewModel.cs
@@ -0,0 +1,26 @@
+﻿using RichCanvasUITests.App.TestMocks;
+using System.Windows.Input;
+
+namespace RichCanvasUITests.App.States
+{
+    public class MultipleSelectionStateViewModel
+    {
+        public MainWindowViewModel Parent { get; }
+
+        public MultipleSelectionStateViewModel(MainWindowViewModel parent)
+        {
+            Parent = parent;
+        }
+
+        private RelayCommand _addSelectableItemsCommmand;
+        public ICommand AddSelectableItemsCommmand => _addSelectableItemsCommmand ??= new RelayCommand(AddSelectableItems);
+
+        private void AddSelectableItems()
+        {
+            foreach (var item in MultipleSelectionStateDataMocks.PositionedSelectableItemsListMock)
+            {
+                Parent.Items.Add(item);
+            }
+        }
+    }
+}
