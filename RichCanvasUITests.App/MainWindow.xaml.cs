442b05d061309f82a671df47ceb1fdb9eab15ed2
Mircea Staicu <staicumircea21@gmail.com>
2024-09-10 18:59:49 +0300

    Add final selection state tests + other selection fixes

--- RichCanvasUITests.App/MainWindow.xaml.cs
+++ RichCanvasUITests.App/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RichCanvasUITests.App.TestMocks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RichCanvasUITests.App
 {
@@ -23,6 +11,14 @@ namespace RichCanvasUITests.App
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void RichItemContainer_Selected(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void RichItemContainer_Unselected(object sender, RoutedEventArgs e)
+        {
         }
     }
 }
