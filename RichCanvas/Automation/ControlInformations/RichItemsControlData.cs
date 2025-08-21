8be47b501e05c2a24f972107eed3881db343715f
Mircea Staicu <staicumircea21@gmail.com>
2025-08-21 15:37:52 +0300

    Fix UI Automation tests data deserialization by making fields public.

--- RichCanvas/Automation/ControlInformations/RichItemsControlData.cs
+++ RichCanvas/Automation/ControlInformations/RichItemsControlData.cs
@@ -10,61 +10,61 @@ namespace RichCanvas.Automation.ControlInformations
         /// <summary>
         /// <see cref="RichCanvas.TranslateTransform"/>.X property value.
         /// </summary>
-        public double TranslateTransformX { get; internal set; }
+        public double TranslateTransformX { get; set; }
 
         /// <summary>
         /// <see cref="RichCanvas.TranslateTransform"/>.Y property value.
         /// </summary>
-        public double TranslateTransformY { get; internal set; }
+        public double TranslateTransformY { get; set; }
 
         /// <summary>
         /// <see cref="RichCanvas.ItemsExtent"/> property value.
         /// </summary>
-        public Rect ItemsExtent { get; internal set; }
+        public Rect ItemsExtent { get; set; }
 
         /// <summary>
         /// <see cref="RichCanvas.ScrollFactor"/> property value.
         /// </summary>
-        public double ScrollFactor { get; internal set; }
+        public double ScrollFactor { get; set; }
 
         /// <summary>
         /// <see cref="RichCanvas.ViewportLocation"/> property value.
         /// </summary>
-        public Point ViewportLocation { get; internal set; }
+        public Point ViewportLocation { get; set; }
 
         /// <summary>
         /// <see cref="RichCanvas.ViewportSize"/> property value.
         /// </summary>
-        public Size ViewportSize { get; internal set; }
+        public Size ViewportSize { get; set; }
 
         /// <summary>
         /// Size value from <see cref="RichCanvas.ExtentWidth"/> and <see cref="RichCanvas.ExtentHeight"/> properties values.
         /// </summary>
-        public Size ViewportExtent { get; internal set; }
+        public Size ViewportExtent { get; set; }
 
         /// <summary>
         /// <see cref="RichCanvas.ViewportZoom"/> property value.
         /// </summary>
-        public double ViewportZoom { get; internal set; }
+        public double ViewportZoom { get; set; }
 
         /// <summary>
         /// <see cref="RichCanvas.ScaleFactor"/> property value.
         /// </summary>
-        public double ScaleFactor { get; internal set; }
+        public double ScaleFactor { get; set; }
 
         /// <summary>
         /// <see cref="RichCanvas.MousePosition"/> property value.
         /// </summary>
-        public Point MousePosition { get; internal set; }
+        public Point MousePosition { get; set; }
 
         /// <summary>
         /// <see cref="RichCanvas.MaxScale"/> property value.
         /// </summary>
-        public double MaxZoom { get; internal set; }
+        public double MaxZoom { get; set; }
 
         /// <summary>
         /// <see cref="RichCanvas.MinScale"/> property value.
         /// </summary>
-        public double MinZoom { get; internal set; }
+        public double MinZoom { get; set; }
     }
 }
