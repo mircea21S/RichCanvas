e2411ceb27895987215546d7cc10a758c90e75bc
Mircea Staicu <staicumircea21@gmail.com>
2025-12-22 17:54:27 +0200

    Created Overlay custom control. Use it over RichCanvas to show settings, features and items preview.

---
+++ Automation Tests/RichCanvasUIA.Client/Debug Mode/Overlay/OverlayHost.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace RichCanvasUIA.Client.Debug_Mode.Overlay
+{
+    /// <summary>
+    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
+    ///
+    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:RichCanvasUIA.Client.Debug_Mode.Overlay"
+    ///
+    ///
+    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
+    /// Add this XmlNamespace attribute to the root element of the markup file where it is
+    /// to be used:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:RichCanvasUIA.Client.Debug_Mode.Overlay;assembly=RichCanvasUIA.Client.Debug_Mode.Overlay"
+    ///
+    /// You will also need to add a project reference from the project where the XAML file lives
+    /// to this project and Rebuild to avoid compilation errors:
+    ///
+    ///     Right click on the target project in the Solution Explorer and
+    ///     "Add Reference"->"Projects"->[Browse to and select this project]
+    ///
+    ///
+    /// Step 2)
+    /// Go ahead and use your control in the XAML file.
+    ///
+    ///     <MyNamespace:OverlayHost/>
+    ///
+    /// </summary>
+
+    [TemplatePart(Name = LayoutRootPart, Type = typeof(Grid))]
+    [TemplatePart(Name = OverlayPart, Type = typeof(ContentPresenter))]
+    [TemplatePart(Name = ContentPart, Type = typeof(ContentPresenter))]
+    public class OverlayHost : ContentControl
+    {
+        private const string ContentPart = "PART_Content";
+        private const string OverlayPart = "PART_Overlay";
+        private const string LayoutRootPart = "PART_LayoutRoot";
+
+        public static DependencyProperty OverlayProperty = DependencyProperty.Register(nameof(Overlay), typeof(UIElement), typeof(OverlayHost), new FrameworkPropertyMetadata(default(UIElement)));
+
+        public UIElement Overlay
+        {
+            get => (UIElement)GetValue(OverlayProperty);
+            set => SetValue(OverlayProperty, value);
+        }
+
+        static OverlayHost()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(OverlayHost), new FrameworkPropertyMetadata(typeof(OverlayHost)));
+        }
+    }
+}
---
+++ Automation Tests/RichCanvasUIA.Client/Debug Mode/Overlay/RichCanvasToolbox.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+using RichCanvasUIA.Client.Debug_Mode.Editor;
+
+namespace RichCanvasUIA.Client.Debug_Mode.Overlay
+{
+    /// <summary>
+    /// Interaction logic for RichCanvasToolbox.xaml
+    /// </summary>
+    public partial class RichCanvasToolbox : UserControl
+    {
+        public static DependencyProperty RichCanvasEditorProperty = DependencyProperty.Register(nameof(RichCanvasEditor), typeof(RichCanvasEditor), typeof(RichCanvasToolbox), new FrameworkPropertyMetadata(default(RichCanvasEditor)));
+
+        public RichCanvasEditor RichCanvasEditor
+        {
+            get => (RichCanvasEditor)GetValue(RichCanvasEditorProperty);
+            set => SetValue(RichCanvasEditorProperty, value);
+        }
+
+        public RichCanvasToolbox()
+        {
+            InitializeComponent();
+        }
+    }
+}
